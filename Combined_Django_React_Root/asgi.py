6631528a055a3e42e7e7a76c220194f0b484416f
Sachin <sachnaror@gmail.com>
2024-09-15 19:07:59 +0530

    Initial commit

---
+++ Combined_Django_React_Root/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Combined_Django_React_Root project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Combined_Django_React_Root.settings')
+
+application = get_asgi_application()
---
+++ README.md
@@ -0,0 +1,4 @@
+# Combined_Django_React_Root
+
+## Description
+A Django project with the app Django_App.
